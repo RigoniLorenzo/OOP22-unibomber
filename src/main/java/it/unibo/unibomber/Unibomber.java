96f08ad390164370c03bcc04d2738c14d648b908
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2023-02-27 08:47:07 +0100

    Setup gradle

---
+++ src/main/java/it/unibo/unibomber/Unibomber.java
@@ -0,0 +1,30 @@
+package it.unibo.unibomber;
+
+import processing.core.*;
+
+public class Unibomber extends PApplet{
+    private boolean setup = false;
+
+    public void settings() {
+        size(400, 400);
+    }
+
+    public void setup() {
+        this.setup = true;
+        background(220);
+        if (this.setup) {}
+    }
+
+    public void draw() {
+
+    }
+
+    /**
+     * 
+     */
+    public static void main(String[] passedArgs) {
+        String[] processingArgs = {"Unibomber"};
+        final Unibomber uniBomber = new Unibomber();
+        PApplet.runSketch(processingArgs, uniBomber);
+    }
+}
