e9dd9d04e3252dffac6cdb1058f55ea1e5dfd33e
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2023-02-28 19:21:09 +0100

    Added and implemented the World Interface

--- src/main/java/it/unibo/unibomber/game/model/api/World.java
+++ src/main/java/it/unibo/unibomber/game/model/api/World.java
@@ -1,5 +1,31 @@
 package it.unibo.unibomber.game.model.api;
 
+import java.util.List;
+import java.util.Map;
+
+import it.unibo.unibomber.game.ecs.api.Entity;
+
 public interface World {
     
+    /**
+     * @return the list of all entities in the current world
+     */
+    List<Entity> getEntities();
+
+    /**
+     * @param <C> only extension of entity
+     * @param entity the entity to be added to the World
+     */
+    <C extends Entity> void addEntity(C entity);
+
+    /**
+     * @return all keys currently pressed
+     */
+    Map<Character,Boolean> getPressedKeys();
+
+    /**
+     * @param keyCode the actual code of the keyboard key
+     * @param isPressed whether it was pressed or released
+     */
+    void putCharacterPressed(char keyCode, boolean isPressed);
 }
---
+++ src/main/java/it/unibo/unibomber/game/model/impl/WorldImpl.java
@@ -0,0 +1,39 @@
+package it.unibo.unibomber.game.model.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import it.unibo.unibomber.game.ecs.api.Entity;
+import it.unibo.unibomber.game.model.api.World;
+
+public class WorldImpl implements World {
+
+    private List<Entity> entities = new ArrayList<>();
+    private Map<Character,Boolean> keysPressed = new HashMap<>();  
+
+    @Override
+    public List<Entity> getEntities() {
+        return this.entities;
+    }
+
+    @Override
+    public <C extends Entity> void addEntity(C entity) {
+        entities.add(entity);
+    }
+
+    @Override
+    public boolean isBeingPressed(char keyCode) {
+        return keysPressed.containsKey(keyCode);
+    }
+
+    @Override
+    public void putCharacterPressed(char keyCode, boolean isPressed) {
+        keysPressed.put(keyCode, isPressed);
+    }
+
+
+    
+    
+}
