3ad34cdae3e663af71a94bcb6b4057b6be40ffd4
Alex <alex.tamagnini@studio.unibo.it>
2023-02-28 09:17:50 +0100

    Added the skeleton of PowerUpFactoryImpl

---
+++ src/main/java/it/unibo/unibomber/game/ecs/impl/PowerUpFactoryImpl.java
@@ -0,0 +1,62 @@
+package it.unibo.unibomber.game.ecs.impl;
+
+import it.unibo.unibomber.game.ecs.api.Component;
+import it.unibo.unibomber.game.ecs.api.PowerUpFactory;
+
+public class PowerUpFactoryImpl implements PowerUpFactory {
+
+    @Override
+    public Component makeBombKickPowerUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makeBombKickPowerUp'");
+    }
+
+    @Override
+    public Component makeBombThrowPowerUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makeBombThrowPowerUp'");
+    }
+
+    @Override
+    public Component makeSpikedBombPowerUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makeSpikedBombPowerUp'");
+    }
+
+    @Override
+    public Component makePowerfullBombPowerUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makePowerfullBombPowerUp'");
+    }
+
+    @Override
+    public Component makebouncyBombPowerUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makebouncyBombPowerUp'");
+    }
+
+    @Override
+    public Component makelineBombPowerUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makelineBombPowerUp'");
+    }
+
+    @Override
+    public Component makemineBombPowerUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makemineBombPowerUp'");
+    }
+
+    @Override
+    public Component makeshieldPowerUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makeshieldPowerUp'");
+    }
+
+    @Override
+    public Component makebasicPowerUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makebasicPowerUp'");
+    }
+    
+}
