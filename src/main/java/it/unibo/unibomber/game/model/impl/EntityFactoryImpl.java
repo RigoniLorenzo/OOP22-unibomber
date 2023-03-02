b7d11db6b38277aa9e5a44eb11b73deaf9f84085
Riccardo Moretti <riccardo.moretti6@studio.unibo.it>
2023-03-02 19:28:16 +0100

    refactoring of the powerUp handling

--- src/main/java/it/unibo/unibomber/game/model/impl/EntityFactoryImpl.java
+++ src/main/java/it/unibo/unibomber/game/model/impl/EntityFactoryImpl.java
@@ -6,39 +6,34 @@ import it.unibo.unibomber.utilities.Pair;
 
 public class EntityFactoryImpl implements EntityFactory{
 
+    //TODO everything
     @Override
     public Entity makeBomber(Pair<Float, Float> position) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'makePlayable'");
     }
 
     @Override
     public Entity makePlayable(Pair<Float, Float> position) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'makePlayable'");
     }
 
     @Override
     public Entity makeBot(Pair<Float, Float> position) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'makeBot'");
     }
 
     @Override
     public Entity makeBomb(Pair<Float, Float> position, Entity placer) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'makeBomb'");
     }
 
     @Override
     public Entity makeDestructibleWall(Pair<Float, Float> position) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'makeDestructibleWall'");
     }
 
     @Override
     public Entity makeIndestructibleWall(Pair<Float, Float> position) {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'makeIndestructibleWall'");
     }
     
