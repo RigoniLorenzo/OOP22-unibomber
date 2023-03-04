db021d4a2757fc352ccfc38e37ca6948eefd56e4
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2023-03-04 22:48:12 +0100

    Moved WallFactory methods in EntityFactory

--- src/main/java/it/unibo/unibomber/game/model/api/EntityFactory.java
+++ src/main/java/it/unibo/unibomber/game/model/api/EntityFactory.java
@@ -12,7 +12,7 @@ public interface EntityFactory {
      * @param powerUpType the Type of the PowerUp
      * @return the PowerUp described
      */
-    public Entity makePowerUp(Pair<Float, Float> pos, PowerUpType powerUpType);
+    Entity makePowerUp(Pair<Float, Float> pos, PowerUpType powerUpType);
     /**
      * @param coordinates the initial position 
      * @param type whether it is a playable character or a bot
@@ -23,26 +23,26 @@ public interface EntityFactory {
      * @param coordinates the initial position 
      * @return an instance of a Playable Bomber
      */
-    public Entity makePlayable(Pair<Float,Float> position);
+    Entity makePlayable(Pair<Float,Float> position);
     /**
      * @param coordinates the initial position 
      * @return an instance of a non playable Bomber
      */
-    public Entity makeBot(Pair<Float, Float> position, int AI_difficulty);
-        /**
+    Entity makeBot(Pair<Float, Float> position, int AI_difficulty);
+    /**
      * @param placer the reference to the entity which placed the bomb
      *               in normal condition being the bomber
      * @return an instance of a non an instance of a non playable Bomb
      */
-    public Entity makeBomb(Entity placer);
+    Entity makeBomb(Entity placer);
     /**
      * @param position the initial position
      * @return an instance of a non playable a Wall which can be destroyed
      */
-    public Entity makeDestructibleWall(Pair<Float,Float> position);
+    Entity makeDestructibleWall(Pair<Float,Float> position);
     /**
      * @param position the initial position
      * @return an instance of a wall which cannot be destroyed
      */
-    public Entity makeIndestructibleWall(Pair<Float,Float> position);
+    Entity makeIndestructibleWall(Pair<Float,Float> position);
 }
